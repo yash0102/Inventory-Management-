e8fbf7a8bea80604ea9322409272a91e6a6880a9
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-08-23 18:18:11 +0530

    Creating Model

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,8 +1,10 @@
 import path from 'path';
+import ProductModel from '../models/product.model.js';
 
 export default class ProductController {
     getProducts (req, res) {
-        console.log(path.resolve());
+        let products = ProductModel.get();
+        console.log(products);
         return res.sendFile(path.join(path.resolve(), 'src', 'views', 'products.html'))
     }
 }
