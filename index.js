0cd4f45a38cb3d01faba92ff27158d3e89421488
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-09-16 19:03:18 +0530

    Regitration Page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import path from 'path';
 import ProductController from "./src/controllers/product.controller.js";
 import validateProduct from "./src/middlewares/validation.middleware.js";
 import { uploadFile } from "./src/middlewares/file-upload.middleware.js";
+import UserController from "./src/controllers/user.controller.js";
 
 
 const server = express();
@@ -22,12 +23,15 @@ server.set("views", path.join(path.resolve(), "src", "views"));
 
 // Create an instance of ProductController
 const productController = new ProductController()
+const usersController = new UserController();
+
 server.get('/', productController.getProducts)
 server.get("/new", productController.getAddForm);
 server.post("/", uploadFile.single('imageUrl'), validateProduct, productController.addNewProduct);
 server.get("/update-product/:id", productController.getUpdateProductView);
 server.post("/update-product", productController.postUpdateProduct );
 server.post("/delete-product/:id", productController.deleteProduct);
+server.get("/register",usersController.getRegister);
 
 server.listen(3400 , ()=> {
     console.log('Server in running on port 3400');
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export default class UserController {
+    getRegister(req, res) {
+        res.render('register');
+    }
+}
